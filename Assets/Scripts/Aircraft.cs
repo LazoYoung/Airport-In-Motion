122ea003a8bbb3f981b457eb831cf9d6f92d4529
Chanyoung Park <idealproduction@naver.com>
2024-04-25 17:44:16 +0900

    Taxiway intercept logic

---
+++ Assets/Scripts/Aircraft.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Dreamteck.Splines;
+using UnityEngine;
+
+[RequireComponent(typeof(SplineFollower))]
+public class Aircraft : MonoBehaviour
+{
+    // todo apply changes to SplineFollower
+    [SerializeField] private float taxiSpeed = 10f;
+    [SerializeField] private float turnRadius = 30f;
+    [HideInInspector] [SerializeField] private SplineFollower follower;
+    [HideInInspector] [SerializeField] private SplineComputer taxiway;
+    [HideInInspector] [SerializeField] private Spline.Direction taxiDirection;
+
+    private void Awake()
+    {
+        tag = "Aircraft";
+        follower = GetComponent<SplineFollower>();
+        follower.follow = false;
+    }
+
+    public void JoinTaxiway(SplineComputer spline, Spline.Direction direction)
+    {
+        var sample = new SplineSample();
+        taxiDirection = direction;
+        taxiway = spline;
+        taxiway.Project(transform.position, ref sample);
+        
+        var inbound = (sample.position - transform.position).normalized;
+        var join = sample.position;
+        var preJoin = join - turnRadius * inbound;
+        taxiway.Evaluate(taxiway.Travel(sample.percent, turnRadius, direction), ref sample);
+        var postJoin = sample.position;
+        var points = new SplinePoint[4];
+        points[0] = new SplinePoint(transform.position);
+        points[1] = new SplinePoint(preJoin);
+        points[2] = new SplinePoint(join);
+        points[3] = new SplinePoint(postJoin);
+
+        var tmpSpline = gameObject.AddComponent<SplineComputer>();
+        tmpSpline.sampleMode = SplineComputer.SampleMode.Uniform;
+        tmpSpline.sampleRate = 20;
+        tmpSpline.type = Spline.Type.BSpline;
+        tmpSpline.space = SplineComputer.Space.World;
+        tmpSpline.SetPoints(points);
+
+        follower.spline = tmpSpline;
+        follower.RebuildImmediate();
+
+        follower.direction = Spline.Direction.Forward;
+        follower.wrapMode = SplineFollower.Wrap.Default;
+        follower.followMode = SplineFollower.FollowMode.Uniform;
+        follower.followSpeed = taxiSpeed;
+        follower.follow = true;
+        follower.onEndReached += OnTaxiwayJoin;
+        Debug.Log("join twy");
+    }
+
+    public void StartTaxi(SplineComputer spline, Spline.Direction direction)
+    {
+        taxiway = spline;
+        taxiDirection = direction;
+        StartTaxi();
+    }
+    
+    private void StartTaxi()
+    {
+        follower.spline = taxiway;
+        follower.RebuildImmediate();
+
+        var pos = transform.position;
+        var sample = follower.spline.Project(pos);
+        follower.SetPercent(sample.percent);
+        follower.direction = taxiDirection;
+        follower.wrapMode = SplineFollower.Wrap.Default;
+        follower.followMode = SplineFollower.FollowMode.Uniform;
+        follower.followSpeed = taxiSpeed;
+        follower.follow = true;
+        Debug.Log($"taxi start at {sample.percent}");
+    }
+
+    private void OnTaxiwayJoin(double d)
+    {
+        follower.onEndReached -= OnTaxiwayJoin;
+        follower.follow = false;
+        Destroy(follower.spline);
+        StartTaxi();
+    }
+}
---
+++ Assets/Scripts/Editor/JoinTaxiwayButton.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Generic;
+using Dreamteck.Splines;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Editor
+{
+    [CustomEditor(typeof(Aircraft))]
+    public class JoinTaxiwayButton : UnityEditor.Editor
+    {
+        public override VisualElement CreateInspectorGUI()
+        {
+            var root = GetRootElement();
+            var taxiways = FindTaxiways();
+            var box = new Box()
+            {
+                style =
+                {
+                    marginTop = 10f,
+                    paddingTop = 10f,
+                    paddingBottom = 10f,
+                    paddingLeft = 10f,
+                    paddingRight = 10f
+                }
+            };
+            var label = new Label("Actions")
+            {
+                style =
+                {
+                    fontSize = 14,
+                    unityFontStyleAndWeight = FontStyle.Bold,
+                    unityTextAlign = TextAnchor.MiddleCenter,
+                    paddingBottom = 20f
+                }
+            };
+            var actions = new VisualElement()
+            {
+                style = { flexDirection = FlexDirection.Row }
+            };
+            var dropdown = GetTaxiwayDropdown(taxiways);
+            var button = GetJoinTaxiwayButton();
+            var aircraft = target as Aircraft;
+            
+            button.clicked += () =>
+            {
+                if (!Application.isPlaying)
+                {
+                    Debug.LogWarning("Game is not running!");
+                    return;
+                }
+                
+                var taxiway = taxiways[dropdown.index];
+                var direction = Spline.Direction.Forward;
+                aircraft!.JoinTaxiway(taxiway, direction);
+            };
+            actions.Add(dropdown);
+            actions.Add(button);
+            box.Add(label);
+            box.Add(actions);
+            root.Add(box);
+            return root;
+        }
+
+        private VisualElement GetRootElement()
+        {
+            var root = new VisualElement();
+            var iter = serializedObject.GetIterator();
+
+            for (var enterChildren = true; iter.NextVisible(enterChildren); enterChildren = false)
+            {
+                var propertyField = new PropertyField(iter);
+                propertyField.Bind(serializedObject);
+                root.Add(propertyField);
+            }
+
+            return root;
+        }
+
+        private Button GetJoinTaxiwayButton()
+        {
+            return new Button
+            {
+                text = "Join Taxiway",
+                style =
+                {
+                    flexGrow = 1f
+                }
+            };
+        }
+
+        private DropdownField GetTaxiwayDropdown(SplineComputer[] taxiways)
+        {
+            var choices = new List<string>(taxiways.Length);
+            foreach (var spline in taxiways)
+            {
+                choices.Add(spline.name);
+            }
+            
+            return new DropdownField(choices, 0)
+            {
+                tooltip = "Select a taxiway.",
+                style =
+                {
+                    marginRight = 20f,
+                    width = 60f
+                }
+            };
+        }
+
+        private SplineComputer[] FindTaxiways()
+        {
+            var splines = FindObjectsOfType<SplineComputer>();
+            var result = new List<SplineComputer>();
+            
+            foreach (var spline in splines)
+            {
+                if (!spline.CompareTag("Aircraft"))
+                {
+                    result.Add(spline);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
